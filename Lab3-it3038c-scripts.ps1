8954ca5e317d1adb52a35562af65b0a4270c299c
juharan <141169523+juharan@users.noreply.github.com>
2023-09-24 21:52:06 -0400

    Add files via upload

---
+++ Lab3-it3038c-scripts.ps1
@@ -0,0 +1,10 @@
+﻿function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"}
+$IP = getIP
+$user = $env:username 
+$ver = get-host | Select-Object Version
+$host = $env:COMPUTERNAME
+$date = get-date -DisplayHint Date
+$body = "This machine's IP is $IP, User is $User, Hostname is $HOST, PowerShell version is $ver, Today's Date is $DATE."
+write-Host($body)
+Send-MailMessage -To "botheaj@ucmail.uc.edu" -From "aa.juhar@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
