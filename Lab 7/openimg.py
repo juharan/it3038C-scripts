5962d08fad279cbaaecf67f510a28852ef91d559
juharan <141169523+juharan@users.noreply.github.com>
2023-10-16 04:43:47 -0400

    Update openimg.py

--- Lab 7/openimg.py
+++ Lab 7/openimg.py
@@ -1,1 +1,13 @@
 
+
+>>> from PIL import Image
+>>> filename = "buildings.jpg"
+>>> with Image.open(filename) as img:
+...     img.load()
+...
+
+>>> type(img)
+<class 'PIL.JpegImagePlugin.JpegImageFile'>
+
+>>> isinstance(img, Image.Image)
+True
