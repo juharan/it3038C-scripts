1e698a6bb2218a4e7f28e182f8102ca7471558d7
juharan <141169523+juharan@users.noreply.github.com>
2023-10-16 04:44:53 -0400

    Update resize.py
    Lab 7 resize and cropping

--- Lab 7/resize.py
+++ Lab 7/resize.py
@@ -1,1 +1,11 @@
 
+>>> cropped_img = img.crop((300, 150, 700, 1000))
+>>> cropped_img.size
+(400, 850)
+
+>>> cropped_img.show()
+
+>>> low_res_img = cropped_img.resize(
+...     (cropped_img.width // 4, cropped_img.height // 4)
+... )
+>>> low_res_img.show()
